114aa579f52b4bfa2c04e31565907ee961cdff00
Sêneca Brito <limaseneca@gmail.com>
2026-07-22 18:46:08 -0300

    Initial Commit

---
+++ src/main/java/com/seneca_brito/lab_manager/LabManagerApplication.java
@@ -0,0 +1,13 @@
+package com.seneca_brito.lab_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seneca_brito/lab_manager/LabManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seneca_brito.lab_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
