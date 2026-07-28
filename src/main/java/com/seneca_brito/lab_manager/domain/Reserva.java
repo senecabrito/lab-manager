535dd7ad3de5943d479b0aeb768c6926eb924115
Florido-hub <floridodinizz@gmail.com>
2026-07-28 19:47:12 -0300

    feat: cria pojo entidade reserva basico

--- src/main/java/com/seneca_brito/lab_manager/domain/Reserva.java
+++ src/main/java/com/seneca_brito/lab_manager/domain/Reserva.java
@@ -1,4 +1,43 @@
 package com.seneca_brito.lab_manager.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table(name = "Reserva")
+@Data
 public class Reserva {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @JdbcTypeCode(SqlTypes.BINARY)
+    private UUID id;
+
+    @Column(name = "data_reserva")
+    private LocalDate dataReserva;
+
+    @Column(name = "horario_inicio")
+    private LocalDateTime horarioInicio;
+
+    @Column(name = "horario_fim")
+    private LocalDateTime horarioFim;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Reserva reserva = (Reserva) o;
+        return Objects.equals(id, reserva.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
---
+++ src/main/java/com/seneca_brito/lab_manager/infrastructure/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package com.seneca_brito.lab_manager.infrastructure.repositories;
+
+import com.seneca_brito.lab_manager.domain.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+}
