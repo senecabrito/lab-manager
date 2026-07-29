237b5a82b1416640ae9bf2fe5bb2d63b528c850a
Florido-hub <floridodinizz@gmail.com>
2026-07-29 11:19:54 -0300

    feat: implementa entidade laboratorio

---
+++ src/main/java/com/seneca_brito/lab_manager/shared/ENUM/TipoLaboratorio.java
@@ -0,0 +1,13 @@
+package com.seneca_brito.lab_manager.shared.ENUM;
+
+public enum TipoLaboratorio {
+    INFORMATICA,
+    QUIMICA,
+    FISICA,
+    BIOLOGIA,
+    ROBOTICA,
+    ELETRONICA,
+    MULTIDISCIPLINAR,
+    INDIOMAS,
+    OUTRO
+}
