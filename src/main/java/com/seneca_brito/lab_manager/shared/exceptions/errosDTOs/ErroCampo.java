0dfc78049896d46757dc17fda056a982e4ba04db
Florido-hub <floridodinizz@gmail.com>
2026-07-30 18:49:51 -0300

    chore: criando padronizacao de respostas de exceções

---
+++ src/main/java/com/seneca_brito/lab_manager/shared/exceptions/errosDTOs/ErroCampo.java
@@ -0,0 +1,7 @@
+package com.seneca_brito.lab_manager.shared.exceptions.errosDTOs;
+
+public record ErroCampo(
+        String campo,
+        String erro
+) {
+}
---
+++ src/main/java/com/seneca_brito/lab_manager/shared/exceptions/errosDTOs/ErroResponse.java
@@ -0,0 +1,17 @@
+package com.seneca_brito.lab_manager.shared.exceptions.errosDTOs;
+
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+public record ErroResponse(
+        int status,
+        String message,
+        List<ErroCampo> erro
+) {
+    public static ErroResponse respostaPadrao(String mensagem){
+        return new ErroResponse(HttpStatus.BAD_REQUEST.value(), mensagem, List.of());
+    }
+
+
+}
