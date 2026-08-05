57e0081553f60c5a6159c9fc8d778fe8b9e77c82
Florido-hub <floridodinizz@gmail.com>
2026-08-05 16:50:44 -0300

    feat: implementa contrato de criação de laboratorio

--- src/main/java/com/seneca_brito/lab_manager/application/Commands/laboratorio/create/CreateLaboratorioCommand.java
+++ src/main/java/com/seneca_brito/lab_manager/application/Commands/laboratorio/create/CreateLaboratorioCommand.java
@@ -1,4 +1,32 @@
 package com.seneca_brito.lab_manager.application.Commands.laboratorio.create;
 
+import com.seneca_brito.lab_manager.domain.Laboratorio;
+import com.seneca_brito.lab_manager.shared.DTOs.laboratorioDTOs.LaboratorioRequestDTO;
+import com.seneca_brito.lab_manager.shared.mappers.LaboratorioMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/laboratorios")
+@RequiredArgsConstructor
 public class CreateLaboratorioCommand {
+
+    private final CreateLaboratorioHandler laboratorioHandler;
+    private final LaboratorioMapper laboratorioMapper;
+
+    @PostMapping
+    public ResponseEntity<Void> createLaboratorio(@RequestBody LaboratorioRequestDTO laboratorioDTO) {
+        Laboratorio laboratorio = laboratorioMapper.toModel(laboratorioDTO);
+
+        Laboratorio response = laboratorioHandler.create(laboratorio);
+        String id = response.getId().toString();
+
+        return ResponseEntity.created(URI.create(id)).build();
+    }
 }
--- src/main/java/com/seneca_brito/lab_manager/application/Commands/laboratorio/create/CreateLaboratorioHandler.java
+++ src/main/java/com/seneca_brito/lab_manager/application/Commands/laboratorio/create/CreateLaboratorioHandler.java
@@ -1,5 +1,18 @@
 package com.seneca_brito.lab_manager.application.Commands.laboratorio.create;
 
-public class CreateLaboratorioHandler
-{
+import com.seneca_brito.lab_manager.domain.Laboratorio;
+import com.seneca_brito.lab_manager.infrastructure.repositories.LaboratorioRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CreateLaboratorioHandler {
+
+    private final LaboratorioRepository laboratorioRepository;
+
+
+    public Laboratorio create(Laboratorio laboratorio) {
+        return laboratorioRepository.save(laboratorio);
+    }
 }
