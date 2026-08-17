32c42f1e68be4354ead4344fd2ad34bdb0834b6a
Florido-hub <floridodinizz@gmail.com>
2026-08-16 22:26:30 -0300

    fix: correcoes basicas no controller

--- src/main/java/com/seneca_brito/lab_manager/application/Commands/autenticacao/login/LoginCommand.java
+++ src/main/java/com/seneca_brito/lab_manager/application/Commands/autenticacao/login/LoginCommand.java
@@ -5,7 +5,10 @@ import com.seneca_brito.lab_manager.shared.DTOs.loginDTOs.TokenResponseDTO;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/autenticacao")
@@ -15,9 +18,9 @@ public class LoginCommand {
     private final LoginHandler loginHandler;
 
     @PostMapping("/login")
-    public ResponseEntity<Void> login(@RequestBody @Valid LoginDTO dto) throws Exception {
-        loginHandler.login(dto);
+    public ResponseEntity<TokenResponseDTO> login(@RequestBody @Valid LoginDTO dto) throws Exception {
+        TokenResponseDTO token = loginHandler.login(dto);
 
-        return ResponseEntity.ok().build();
+        return ResponseEntity.ok().body(token);
     }
 }
